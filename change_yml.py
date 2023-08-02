3c7b35867caab1b079ff2ce9ac847a6da3d7bbdf
Sajjad P. Savoji <sj.pakdaman@gmail.com>
2023-08-02 18:06:46 -0400

    Create change_yml.py
    a python file to set a field for yml file

---
+++ change_yml.py
@@ -0,0 +1,28 @@
+import sys
+import yaml
+
+# get file path, field and value from argv
+file_path = sys.argv[0]
+field     = sys.argv[2]
+value     = sys.argv[3]
+
+# read current yml file
+with open(file_path, 'r') as stream:
+    loaded = yaml.safe_load(stream)
+  
+# parse filed by . (eg a.b -> [a, b])
+field_list = list(field.split("."))
+
+# make a nested dict(or update it)
+last_d = loaded
+for i in range(len(field_list) - 1):
+  if not field_list[i] in last_d:
+    last_d[field_list[i]] = {}
+  last_d = last_d[field_list[i]]
+  
+# set value in nested dict
+last_d[field_list[-1]] = value
+
+# write new yml file to path
+with open(file_path, 'w') as stream:
+    yaml.dump(loaded, stream)
